f5c4f2d55a43d9ca048de426cc02496915308fe7
BlueDav <bluedav000@gmail.com>
2016-03-30 17:26:54 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,5 +1,5 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/rockchip/rk29board/device.mk)
